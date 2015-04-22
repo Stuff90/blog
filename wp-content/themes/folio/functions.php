4271eb53c211263d0154b8d67a5f70eab8dbd2a3
Stuff90 <bernard.simon.90@gmail.com>
2015-04-22 17:42:39 +0200

    master  >  Updatyes dependencies in funcitons file

--- wp-content/themes/folio/functions.php
+++ wp-content/themes/folio/functions.php
@@ -1,2 +1,18 @@
-<?php
-    require_once 'class/Content.php';
+<?php require_once 'class/Content.php';
+
+
+
+
+    require_once 'setup/filters.php';
+    add_filter( 'clean_content', 'clean_content_fn', 20 );
+
+
+    function registerMainMenu() {
+        register_nav_menu('header-menu',__( 'Header Menu' ));
+    }
+
+    add_image_size( 'Article Thumbnail' 	, '400' , '250'    , true );
+    add_image_size( 'Article Cover'		    , '900' , '250'    , true );
+
+
+    add_action( 'init', 'registerMainMenu' );
