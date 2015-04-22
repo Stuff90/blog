a7f122530249574de2bc20c05914686f91db2d2a
Stuff90 <bernard.simon.90@gmail.com>
2015-04-22 17:45:34 +0200

    master  >  Update Header to get the WP menu + HTML of search feature

--- wp-content/themes/folio/templates/header.php
+++ wp-content/themes/folio/templates/header.php
@@ -3,3 +3,19 @@
 	    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	</head>
 <body>
+
+	<header id="header">
+		<div class="wrapper">
+			 <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+
+			<aside class="search">
+
+				<p class="search--buttonWrapper"><a href="#" class="search--button"></a></p>
+
+				<form action="#" class="search--form">
+					<input type="text" class="search--input">
+				</form>
+			</aside>
+		</div>
+
+	</header>
