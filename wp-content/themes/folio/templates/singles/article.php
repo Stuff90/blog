aa0d8c8400ad7d1d3c7631fdeb1d43cad6d201e3
ileotech <ileotech@MacBook-Pro-de-ILEO09.local>
2015-04-23 09:44:24 +0200

    master  >  HTML for sidebar => firsts steps

--- wp-content/themes/folio/templates/singles/article.php
+++ wp-content/themes/folio/templates/singles/article.php
@@ -21,7 +21,35 @@
 			<section class="article--content"><?php echo apply_filters('clean_content' , $article->post_content); ?></section>
 		</article>
 
-		<aside class="wrapper-sidebar"><?php echo $article->post_excerpt; ?></aside>
+		<aside class="wrapper-sidebar">
+			<div class="article--sidebar">
+				<ul class="share-articleDetail">
+					<li class="share--item"><a href="#" class="share--link share--facebook" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent('<?php echo get_permalink($article->ID, FALSE); ?>'), 'Share on Facebook','width=626,height=436');return false;"></a></li><!--
+					--><li class="share--item"><a href="#" class="share--link share--twitter" onclick="window.open('https://www.twitter.com/share?text='+encodeURIComponent('@CrFashionBook')+'&url='+encodeURIComponent('<?php echo get_permalink($article->ID, FALSE); ?>'), 'Share on Twitter', 'width=626,height=436');return false;"></a></li>
+				</ul>
+
+				<hr class="bar">
+
+				<h5 class="article--sidebarTitle">Voir aussi :</h5>
+
+				<?php
+
+					$related = new ContentManager();
+					$related
+					    ->setContentType('article')
+					    ->setOrder('DESC')
+					    ->setPostPerPage(4)
+					    ->fetch();
+					?>
+
+					<ul class="article--sidebarRelated">
+						<?php foreach ($related->all() as $relatedArticle) { ?>
+							<li><a href="<?php echo get_permalink($relatedArticle->ID); ?>"><?php echo $relatedArticle->post_title; ?></a></li>
+					   	<?php } ?>
+					</ul>
+
+			</div>
+		</aside>
 
 	</div>
 
